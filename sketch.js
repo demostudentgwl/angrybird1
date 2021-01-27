f90d4da0bfd903fd7ead8d860bf9e809b38362f8
demostudentgwl <demostudentgwl@gmail.com>
2021-01-27 18:51:25 +0530

    Add Platform

--- sketch.js
+++ sketch.js
@@ -14,6 +14,7 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
+    platform = new Ground(150,300,300,17)
 
     
     ground = new Ground(600,height,1200,20)
@@ -59,4 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
